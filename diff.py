230995d10760e5e52368413d7c73089fa7cc86f5
kevteo <31839547+kevteo@users.noreply.github.com>
2021-09-16 16:54:07 +0800

    Update diff.py

--- diff.py
+++ diff.py
@@ -31,39 +31,27 @@ def main():
     merge_strategy = os.environ["INPUT_MERGE_STRATEGY"]
     directory_1 = os.environ["INPUT_JSON1"]
     directory_2 = os.environ["INPUT_JSON2"]
-
+    
     print('start')
     print(merge_strategy)
-    print1 = f"Merge Strategy: {merge_strategy}"
-    print(f"::set-output name=myOutput::{print1}")
-
-    print2 = f"Merge Strategy: {merge_strategy}"
-    print(f"::set-output name=myOutput::{print2}")
-
-    print3 = f"Merge Strategy: {merge_strategy}"
-    print(f"::set-output name=myOutput::{print3}")
-
+    print(directory_1)
+    print(directory_2)
+    
+    # Read json files & Merge
     json1_list = read_json_folder(directory_1)
     json2_list = read_json_folder(directory_2)
-
     merged_json_1 = merge(json1_list, merge_strategy)
-    # merged_json_2 = merge(json2_list, merge_strategy)
+    merged_json_2 = merge(json2_list, merge_strategy)
+    
+    # Output to workflow variable
+    print(f"::set-output name=merge_strategy::{merge_strategy}")
+    print(f"::set-output name=merge_json_1::{merged_json_1}")
+    print(f"::set-output name=merge_json_2::{merged_json_2}")
 
     # Difference_history & Number of changes per key
     # difference_history = generate_difference_history(json_history_1, json_history_2)
     # num_changes = generate_number_changes(difference_history)
-
-    # Output to file
-    with open('merge_history_1.json', 'w') as fp:
-        json.dump(merged_json_1, fp)
-    # with open('merge_history_2.json', 'w') as fp:
-    #     json.dump(merged_json_2, fp)
-    # with open('diff.json', 'w') as fp:
-    #     json.dump(difference_history, fp)
-    # with open('diff.json', 'w') as fp:
-    #     json.dump(num_changes, fp)
     
-
 
 if __name__ == "__main__":
     main()
