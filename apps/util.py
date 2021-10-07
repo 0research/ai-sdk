11c307e1da5dfad4bca06c47a2d6ea9ea0f142dd
Sagun Garg <75983174+sagungargs15@users.noreply.github.com>
2021-10-08 03:13:08 +0800

    removed erroring imports ALL & MATCH dash dependecies

--- apps/util.py
+++ apps/util.py
@@ -1,7 +1,7 @@
 import json
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State, ALL, MATCH
+from dash.dependencies import Input, Output, State
 
 #import dash_bootstrap_components as dbc
 from app import dbc # https://dash-bootstrap-components.opensource.faculty.ai/docs/quickstart/
