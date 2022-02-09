42028e58c6387e85f466878b3061374a059e69bf
kevteo <kevin900124@gmail.com>
2022-02-09 16:59:23 +0800

    Test heroku

--- app.py
+++ app.py
@@ -7,14 +7,18 @@ import sys
 
 
 # Sentry
-# print("Print Host Name: ", socket.gethostname())
-# sys.stdout.flush()
+print("Print Host Name: ", socket.gethostname())
+
 url = ''
 # Heroku dev environment
 if socket.gethostname() == '67487414-1163-4262-b655-30b9cca6a56a':
+    print('in1')
+    sys.stdout.flush()
     url = 'https://f509336fb98a4a6384eb5118ea40fbd8@o1139317.ingest.sentry.io/6194873'
 # Heroku production environment
 elif socket.gethostname() == 'b6ef2409-f785-4d09-8b77-362c0f446c20':
+    print('in2')
+    sys.stdout.flush()
     url = 'https://49cdab632189486d823e4607a3115664@o1139317.ingest.sentry.io/6195029'
 
 if url != '':
