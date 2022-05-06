4a62eb60f5ac01ff0b18f4e1985df93b495ac086
kevteo <kevin900124@gmail.com>
2022-05-06 13:57:13 +0800

    Test

--- index.py
+++ index.py
@@ -241,7 +241,7 @@ if __name__ == '__main__':
     app.server.add_url_rule("/environment", "environment", view_func=lambda: envdump.run())
 
     port = os.environ.get("PORT", 8050)
-    app.run_server("0.0.0.0", port, debug=False)
+    app.run_server("0.0.0.0", port, debug=True)
 
     
 
